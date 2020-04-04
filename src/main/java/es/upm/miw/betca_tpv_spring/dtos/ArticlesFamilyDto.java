45afa9bd0f1966aa43cea8ba50383e0122336653
Harold Lockward <HLockward12@gmail.com>
2020-04-04 03:48:26 +0200

    fixing ArticlesFamilyDto.java

--- src/main/java/es/upm/miw/betca_tpv_spring/dtos/ArticlesFamilyDto.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/dtos/ArticlesFamilyDto.java
@@ -1,10 +1,7 @@
 package es.upm.miw.betca_tpv_spring.dtos;
 
-import es.upm.miw.betca_tpv_spring.documents.Article;
 import es.upm.miw.betca_tpv_spring.documents.ArticlesFamily;
 import es.upm.miw.betca_tpv_spring.documents.FamilyType;
-
-import java.util.List;
 
 public class ArticlesFamilyDto {
 
@@ -16,10 +13,6 @@ public class ArticlesFamilyDto {
 
     private String description;
 
-    private List<ArticlesFamily> articlesFamilyList;
-
-    private Article article;
-
     public ArticlesFamilyDto() {
     }
 
@@ -30,22 +23,9 @@ public class ArticlesFamilyDto {
     }
 
     public ArticlesFamilyDto(ArticlesFamily articlesFamily) {
-        this.id = articlesFamily.getId();
         this.familyType = articlesFamily.getFamilyType();
         this.reference = articlesFamily.getReference();
         this.description = articlesFamily.getDescription();
-        this.articlesFamilyList = articlesFamily.getArticlesFamilyList();
-        this.article = articlesFamily.getArticle();
-
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getCode() {
@@ -70,30 +50,6 @@ public class ArticlesFamilyDto {
 
     public void setReference(String reference) {
         this.reference = reference;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Article getArticle(){
-        return article;
-    }
-
-    public void setArticle(Article article){
-        this.article = article;
-    }
-
-    public List<ArticlesFamily> getArticlesFamilyList() {
-        return articlesFamilyList;
-    }
-
-    public void setArticlesFamilyList(List<ArticlesFamily> articlesFamilyList) {
-        this.articlesFamilyList = articlesFamilyList;
     }
 
     @Override
