9c9325303a714b9ce8e6ab98b7dac7b9c4c0b8cb
DiegoRiofrio <vinicio1716@gmail.com>
2020-05-28 19:39:47 +0200

    Prueba Diego Riofrio #202

--- src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/TagController.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/TagController.java
@@ -8,6 +8,7 @@ import es.upm.miw.betca_tpv_spring.dtos.TagDto;
 import es.upm.miw.betca_tpv_spring.exceptions.BadRequestException;
 import es.upm.miw.betca_tpv_spring.exceptions.ConflictException;
 import es.upm.miw.betca_tpv_spring.exceptions.NotFoundException;
+import es.upm.miw.betca_tpv_spring.repositories.ArticleReactRepository;
 import es.upm.miw.betca_tpv_spring.repositories.ArticleRepository;
 import es.upm.miw.betca_tpv_spring.repositories.TagReactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +18,18 @@ import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+
 @Controller
 public class TagController {
 
     private TagReactRepository tagReactRepository;
     private PdfService pdfService;
-    private SimpleMongoRepository articleReactRepository;
+    private ArticleReactRepository articleReactRepository;
     private ArticleRepository articleRepository;
 
     @Autowired
-    public TagController(TagReactRepository tagReactRepository, PdfService pdfService, SimpleMongoRepository articleReactRepository, ArticleRepository articleRepository) {
+    public TagController(TagReactRepository tagReactRepository, PdfService pdfService, ArticleReactRepository articleReactRepository, ArticleRepository articleRepository) {
         this.tagReactRepository = tagReactRepository;
         this.pdfService = pdfService;
         this.articleReactRepository = articleReactRepository;
@@ -44,15 +47,10 @@ public class TagController {
                 .switchIfEmpty(Flux.error(new BadRequestException("Bad Request")))
                 .map(TagDto::new);
     }
+
     private Mono<Void> noExistsByIdAssured(String id) {
-        return this.articleReactRepository.existsById(id)
-                .handle((result, sink) -> {
-                    if (Boolean.TRUE.equals(result)) {
-                        sink.error(new ConflictException("Article code (" + id + ")"));
-                    } else {
-                        sink.complete()
-                    }
-                });
+        return this.articleReactRepository.findById(id)
+                .switchIfEmpty(Mono.error(new NotFoundException("Article with Id:" + id)));
     }
 
     public Mono<Tag> createTag(TagCreationDto tagCreationDto) {
@@ -60,42 +58,43 @@ public class TagController {
         articles = tagCreationDto.getArticleList().stream().map(articleDto -> Article.builder(articleDto.getCode())
                 .description(articleDto.getDescription())
                 .build()).toArray(Article[]::new);
-        Arrays.stream(article).for-> {
+        Arrays.stream(articles).forEach(article -> {
+            this.noExistsByIdAssured(article.getCode());
+        });
 
-            this.noExistsByIdAssured(article)
-        })
-        Mono<Void> noExistsByIdAssured = this.noExistsByIdAssured(code);
-        if(tagCreationDto.getArticleList().contains(null))
         Tag tag = new Tag();
         tag.setDescription(tagCreationDto.getDescription());
         tag.setArticleList(articles);
         return tagReactRepository.save(tag);
     }
+
     public Mono<TagDto> updateTag(String description, TagCreationDto tagCreationDto) {
         Article[] articles;
         articles = tagCreationDto.getArticleList().stream().map(articleDto -> Article.builder(articleDto.getCode())
                 .description(articleDto.getDescription())
                 .build()).toArray(Article[]::new);
         Mono<Tag> tag = tagReactRepository.findByDescription(description).
-                switchIfEmpty(Mono.error(new NotFoundException("Tag description ("+ description +")")))
+                switchIfEmpty(Mono.error(new NotFoundException("Tag description (" + description + ")")))
                 .map(tag1 -> {
                     tag1.setId(tagCreationDto.getId());
                     tag1.setDescription(description);
                     tag1.setArticleList(articles);
-                   return tag1;
+                    return tag1;
                 });
 
         return Mono.when(tag).then(this.tagReactRepository.saveAll(tag).next().map(TagDto::new));
 
     }
+
     @Transactional
     public Mono<byte[]> printTag(String id) {
         Mono<Tag> tagReact = tagReactRepository.findById(id).switchIfEmpty(Mono.error(new NotFoundException("Tag code (" + id + ")")));
         return pdfService.generateTag(tagReact);
     }
-    public Mono<Void> deleteTag(String id){
+
+    public Mono<Void> deleteTag(String id) {
         Mono<Tag> tag = this.tagReactRepository.findById(id)
-                .switchIfEmpty(Mono.error(new NotFoundException("Tag id:" + id )));
+                .switchIfEmpty(Mono.error(new NotFoundException("Tag id:" + id)));
         return Mono
                 .when(tag)
                 .then(this.tagReactRepository.deleteById(id));
